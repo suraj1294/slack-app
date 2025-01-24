48f6351f0cf97748b49eac48cfbccae397b997e3
suraj <suraj@funnelstory.io>
2025-01-24 16:39:28 +0530

    remove unused ui components

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -12,12 +12,16 @@ export const Sidebar = () => {
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
       <WorkspaceSwitcher />
-      <SidebarButton icon={Home} label="Home" isActive={pathname.includes("/workspace")} />
-      <SidebarButton icon={MessagesSquare} label="DMs" />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      {/* <SidebarButton icon={MessagesSquare} label="DMs" />
       <SidebarButton icon={Bell} label="Activity" />
-      <SidebarButton icon={MoreHorizontal} label="More" />
+      <SidebarButton icon={MoreHorizontal} label="More" /> */}
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
-        <UserButton />        
+        <UserButton />
       </div>
     </aside>
   );
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,4 +1,10 @@
-import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizontal } from "lucide-react";
+import {
+  AlertTriangle,
+  HashIcon,
+  Loader,
+  MessageSquareText,
+  SendHorizontal,
+} from "lucide-react";
 
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetChannels } from "@/features/channels/api/use-get-channels";
@@ -22,10 +28,18 @@ export const WorkspaceSidebar = () => {
 
   const [_open, setOpen] = useCreateChannelModal();
 
-  const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
-  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
-  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
-  const { data: members, isLoading: membersLoading } = useGetMembers({ workspaceId });
+  const { data: member, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+  const { data: members, isLoading: membersLoading } = useGetMembers({
+    workspaceId,
+  });
 
   if (workspaceLoading || memberLoading) {
     return (
@@ -39,17 +53,18 @@ export const WorkspaceSidebar = () => {
     return (
       <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
         <AlertTriangle className="size-5 text-white" />
-        <p className="text-white text-sm">
-          Workspace not found
-        </p>
+        <p className="text-white text-sm">Workspace not found</p>
       </div>
     );
   }
 
   return (
     <div className="flex flex-col bg-[#5E2C5F] h-full">
-      <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
-      <div className="flex flex-col px-2 mt-3">
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />
+      {/* <div className="flex flex-col px-2 mt-3">
         <SidebarItem
           label="Threads"
           icon={MessageSquareText}
@@ -60,7 +75,7 @@ export const WorkspaceSidebar = () => {
           icon={SendHorizontal}
           id="drafts"
         />
-      </div>
+      </div> */}
       <WorkspaceSection
         label="Channels"
         hint="New channel"
@@ -92,5 +107,5 @@ export const WorkspaceSidebar = () => {
         ))}
       </WorkspaceSection>
     </div>
-  )
+  );
 };
